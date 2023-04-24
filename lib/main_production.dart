201f09e4e8eeb2991b9f94b580af269d85cb3a42
f.grezes <f.grezes@exatech-group.com>
2023-04-24 16:41:42 +0200

    feat: add window_manager to prod main

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -3,13 +3,28 @@ import 'package:injectable/injectable.dart';
 import 'package:lumina_gpt/flavors.dart';
 import 'package:lumina_gpt/injection.dart';
 import 'package:lumina_gpt/presentation/core/app_production.dart';
+import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
   F.appFlavor = Flavor.production;
 
   WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
 
   await configureInjection(Environment.prod);
+
+  const windowOptions = WindowOptions(
+    size: Size(1280, 720),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    title: 'LuminaGPT',
+  );
+
+  await windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
 
   runApp(AppProduction());
 }
